04cbca5f867f02d5c81f29f362a843425e780ddf
DWan-Dall <dwandall@gmail.com>
2024-07-29 23:55:13 -0300

    Projeto finalizado

--- src/main.ts
+++ src/main.ts
@@ -12,31 +12,3 @@ app.use(router);
 app.use(store);
 app.use(BootstrapVue3);
 app.mount('#app');
-
-// import Vue, { createApp } from 'vue'
-// import App from './App.vue';
-// import store from './store'
-// import VueRouter from './router';
-
-
-// createApp(App).use(store).use(VueRouter).mount('#app')
-
-// import Vue, { createApp } from 'vue'
-// import App from './App.vue'
-// import './registerServiceWorker'
-// import router from './router'
-// import store from './store'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// // import './app.scss'
-
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-
-
-// const app = createApp(App)
-// app.use(router)
-// // app.use(BootstrapVue)
-// app.mount('#app')
-
-// // createApp(App).use(store).use(router).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import MainLayout from '@/layouts/MainLayout.vue';
 import ItemForm from '@/components/ItemForm.vue';
 import ItemList from '@/components/ItemList.vue';
 
@@ -22,43 +21,3 @@ const router = createRouter({
 });
 
 export default router;
-
-
-// import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-// import ItemForm from '@/components/ItemForm.vue'
-// import ItemList from '@/components/ItemList.vue'
-
-// const routes: Array<RouteRecordRaw> = [
-//   {
-//     path: '/form',
-//     name: 'form',
-//     component: ItemForm
-//   },
-  // {
-  //   path: '/list',
-  //   name: 'list',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "list" */ '../views/ItemList.vue')
-  // }
-
-
-//   Vue.use(VueRouter);
-
-// const routes = [
-//   { path: '/form', component: FormPage },
-//   { path: '/list', component: ListPage },
-//   { path: '*', redirect: '/list' }
-// ];
-
-
-// ]
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes
-// })
-
-// export default router
